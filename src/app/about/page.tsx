efff4857e32989cd0e64432bd445a3ff1e16a19a
jimao <shihao.gu@perfma.com>
2024-10-21 20:04:41 +0800

    计数器

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,20 +1,27 @@
 import { getRequestContext } from '@cloudflare/next-on-pages';
 
-export const runtime = "edge";
-
-const state: any = { };
-
-function getNum() {
-  return (state.num ?? 0) as number;
-}
-
-function setNum(num: number) {
-  state.num = num;
-}
+export const runtime = 'edge';
 
 export default
 async function About() {
   const env: CloudflareEnv | any = getRequestContext().env;
   const kv = env.jimao;
-  return <div>你好，世界：{await kv.get("count")}</div>;
+
+  async function getCount() {
+    try {
+      const stateText = await kv.get('state');
+      return JSON.parse(stateText).num ?? 0;
+    } catch (error) {
+      console.error(error);
+    }
+    return 0;
+  }
+
+  async function setCount(num: number) {
+    await kv.put('state', JSON.stringify({ num }));
+  }
+
+  await setCount(await getCount() + 1);
+
+  return <div>你好，世界：{await getCount()}</div>;
 }
